82258487797c3822257ee9b6cec7f3e9870607af
Jang Ryeol <ryeol.jang@woowahan.com>
2023-09-17 18:37:31 +0900

    Add docker volume

--- scripts/run_http_docker.sh
+++ scripts/run_http_docker.sh
@@ -4,7 +4,10 @@ cd "$SCRIPT_DIR/.."
 
 set -e
 
+CERT_DIR_HOST=`realpath ~/.wine/drive_c/users/crossover/AppData/LocalLow/NPKI`
+CERT_DIR_CONTAINER="/namu/NPKI"
+
 cargo rustc -p qvopenapi-http --release --target i686-pc-windows-gnu --features "disable-unwind" -- -C "panic=abort"
 docker build . -t qvopenapi-http --platform linux/amd64
 
-docker rm qvopenapi-http || docker run -p 18000:18000 --rm -it --name qvopenapi-http qvopenapi-http
+docker rm qvopenapi-http || docker run -p 18000:18000 -v "${CERT_DIR_HOST}:${CERT_DIR_CONTAINER}" --rm -it --name qvopenapi-http qvopenapi-http
